366c4211b16d4ceeb304bd0bee33aacec4458d1f
Pedro Kenichi Lorandi <pedrokl@hotmail.com>
2022-06-27 15:27:47 -0400

    Updates

---
+++ precache-manifest.b59d27c3b465187bdc195bfaeeccdc7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5918dbe050c72c5c23f",
+    "url": "/react-todolist/static/css/main.e484c3df.chunk.css"
+  },
+  {
+    "revision": "f5918dbe050c72c5c23f",
+    "url": "/react-todolist/static/js/main.f5918dbe.chunk.js"
+  },
+  {
+    "revision": "9e4165ca8babdbf8a651",
+    "url": "/react-todolist/static/js/1.9e4165ca.chunk.js"
+  },
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "07cb190a9c42541ccef85aaf0cdc9660",
+    "url": "/react-todolist/index.html"
+  }
+];
